dfaa57fecbf8244e8a80e8da4ae0e417cdccdf3a
Marcos <mr9521192@gmail.com>
2024-11-11 01:30:31 -0300

    commit

--- app/views.py
+++ app/views.py
@@ -4,17 +4,12 @@ from django.shortcuts import redirect, render
 from .layers.services import services
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
-from layers.transport.transport import getAllImages
-from layers.utilities.translator import fromRequestIntoCard ,fromTemplateIntoCard ,fromRepositoryIntoCard
-
-
 
 def index_page(request):
-    return render(request, 'index.html')   
+    return render(request, 'index.html')
 
 # esta función obtiene 2 listados que corresponden a las imágenes de la API y los favoritos del usuario, y los usa para dibujar el correspondiente template.
 # si el opcional de favoritos no está desarrollado, devuelve un listado vacío.
-
 def home(request):
     images = []
     favourite_list = []
